dc45f03a898147583b24010db0520b26146c1b58
victor arcanjo <vtarcanjo@hotmail.com>
2024-01-30 21:40:12 -0300

    feat: add featureMovie

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { Header } from "./components/Header";
-import './App.css'
-
+import "./App.css";
+import { FeatureMovie } from "./components/FeatureMovie";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [featuredData, setFeaturedData] = useState(null);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      setFeaturedData;
+    };
+    loadAll();
+  }, []);
+
   return (
     <div className="page">
       <Header />
+      {featuredData && <FeatureMovie item={featuredData} />}
     </div>
   );
 };
