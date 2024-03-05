1c65a16e638223deb1f085b80355eaf115b5bb5b
victor arcanjo <vtarcanjo@hotmail.com>
2024-03-04 22:35:46 -0300

    style: fix css in swipper

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,13 @@ import App from "./AppRouter";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+// Import main sass file to apply global styles
+import "./static/sass/style.scss";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
